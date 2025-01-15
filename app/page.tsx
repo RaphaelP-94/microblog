2f4e8694ac258b8d4257706b1acfc192ba7d1d6f
Raphael <petsch.ra@gmail.com>
2025-01-15 11:38:41 +0100

    Added fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,28 @@ export default async function Home() {
     const posts = await prisma.post.findMany({
       where: { published: true },
       include: {
-        author: true,
+        author: {
+          select: {
+            name: true,
+            email: true
+          }
+        },
       },
     });
+
+    if (!posts) {
+      return (
+        <main className="max-w-7xl mx-auto p-6">
+          <div>No posts available</div>
+        </main>
+      );
+    }
 
     return (
       <main className="max-w-7xl mx-auto p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {posts.map((post) => (
-            <Post key={post.id} post={post} />
+            <Post key={post.id} post={{...post, author: {...post.author, id: 0, password: ''}}} />
           ))}
         </div>
       </main>
